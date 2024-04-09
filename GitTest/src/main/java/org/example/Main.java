ae982c36862bb94075a5a1d7ea0dd168a4f2c950
Herberth Cerritos <herberthcerritos@Herberths-MacBook-Air.local>
2024-04-09 15:10:29 -0500

    I finished my thought

--- GitTest/src/main/java/org/example/Main.java
+++ GitTest/src/main/java/org/example/Main.java
@@ -3,5 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("This is my leet new app");
+        System.out.println("It will disrupt all industries");
     }
 }
